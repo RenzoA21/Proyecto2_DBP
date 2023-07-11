2274023cd830dbbeb720ba3d5e376d2c95e393ac
Christian <christian.frisancho@utec.edu.pe>
2023-07-11 12:07:32 -0500

    endpoints de usuario y receta completado casi

---
+++ Tests.py
@@ -0,0 +1,39 @@
+import json
+import unittest
+from app.models import Usuario, Receta
+from app import create_app
+config = {
+    'DATABASE_URI': 'postgresql://postgres:1234@localhost:5432/christian',
+}
+
+
+class Tests(unittest.TestCase):
+    def setUp(self):
+        database_qa = config['DATABASE_URI']
+        self.app = create_app({'database_qa': database_qa})
+        self.client = self.app.test_client()
+
+    def test_create_receta(self):
+        response = self.client.post('/recetas', json={
+            'medicamento': 'Ibuprofeno',
+            'tipo_de_toma': 'Oral',
+            'cantidad': 1,
+            'unidad_medida': 'tableta',
+            'porcentaje': 5.0,
+            'ml_g': 500.0
+        })
+        data = response.get_json()
+
+        self.assertEqual(response.status_code, 201)
+        self.assertTrue(data['success'])
+        self.assertIsNotNone(data['id'])
+        self.assertEqual(data['message'], 'Receta created successfully')
+
+    def test_get_recetas(self):
+        response = self.client.get('/recetas')
+        data = response.get_json()
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(data['success'])
+        self.assertIsNotNone(data['recetas'])
+        self.assertEqual(data['total'], len(data['recetas']))
--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,18 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, abort
 from flask_cors import CORS
-from .models import db, setup_db, Usuario, Receta, Cajero, Delivery
+from .models import db, setup_db, Usuario, Receta
 from utilities.utils import allowed_file
-
+from flask import Blueprint
 import os
 import sys
+
+
+users_bp = Blueprint('/usuarios', __name__)
 
 
 def create_app(test_config=None):
     app = Flask(__name__)
     with app.app_context():
-        app.config['UPLOAD_FOLDER'] = 'static/employees'
         setup_db(app)
         CORS(app, origins='*')
 
@@ -23,149 +25,178 @@ def create_app(test_config=None):
         return response
 
     # Definir los endpoints para cada clase
-
-    @app.route('/usuarios', methods=['GET'])
-    def get_usuarios():
-        usuarios = Usuario.query.all()
-        return jsonify([usuario.serialize() for usuario in usuarios])
-
-    @app.route('/usuarios', methods=['POST'])
-    def create_usuario():
-        data = request.get_json()
-        nuevo_usuario = Usuario(**data)
-        db.session.add(nuevo_usuario)
-        db.session.commit()
-        return jsonify(nuevo_usuario.serialize()), 201
-
-    @app.route('/usuarios/<int:usuario_id>', methods=['PATCH'])
-    def update_usuario(usuario_id):
-        data = request.get_json()
-        usuario = Usuario.query.get(usuario_id)
-        if usuario is None:
-            return jsonify({'error': 'Usuario no encontrado'}), 404
-        for key, value in data.items():
-            if hasattr(usuario, key):
-                setattr(usuario, key, value)
-        db.session.commit()
-        return jsonify(usuario.serialize())
-
-    @app.route('/usuarios/<int:usuario_id>', methods=['DELETE'])
-    def delete_usuario(usuario_id):
-        usuario = Usuario.query.get(usuario_id)
-        if usuario is None:
-            return jsonify({'error': 'Usuario no encontrado'}), 404
-        db.session.delete(usuario)
-        db.session.commit()
-        return jsonify({'success': True})
-
-    @app.route('/api/recetas', methods=['GET'])
-    def get_recetas():
-        recetas = Receta.query.all()
-        return jsonify([receta.serialize() for receta in recetas])
-
-
-    @app.route('/api/recetas', methods=['POST'])
+    @app.route('/recetas', methods=['POST'])
     def create_receta():
-        data = request.get_json()
-        nueva_receta = Receta(**data)
-        db.session.add(nueva_receta)
-        db.session.commit()
-        return jsonify(nueva_receta.serialize()), 201
+        error_code = 201
+        list_errors = []
+        try:
+            body = request.json
 
+            if 'medicamento' not in body:
+                list_errors.append('medicamento is required')
+            else:
+                medicamento = body.get('medicamento')
 
-    @app.route('/api/recetas/<int:receta_id>', methods=['PATCH'])
-    def update_receta(receta_id):
-        data = request.get_json()
-        receta = Receta.query.get(receta_id)
-        if receta is None:
-            return jsonify({'error': 'Receta no encontrada'}), 404
-        for key, value in data.items():
-            setattr(receta, key, value)
-        db.session.commit()
-        return jsonify(receta.serialize())
+            if 'tipo_de_toma' not in body:
+                list_errors.append('tipo_de_toma is required')
+            else:
+                tipo_de_toma = body.get('tipo_de_toma')
 
+            if 'cantidad' not in body:
+                list_errors.append('cantidad is required')
+            else:
+                cantidad = body.get('cantidad')
 
-    @app.route('/api/recetas/<int:receta_id>', methods=['DELETE'])
-    def delete_receta(receta_id):
-        receta = Receta.query.get(receta_id)
-        if receta is None:
-            return jsonify({'error': 'Receta no encontrada'}), 404
-    db.session.delete(receta)
-    db.session.commit()
-    return jsonify({'success': True})
+            if 'unidad_medida' not in body:
+                list_errors.append('unidad_medida is required')
+            else:
+                unidad_medida = body.get('unidad_medida')
 
+            if 'porcentaje' not in body:
+                list_errors.append('porcentaje is required')
+            else:
+                porcentaje = body.get('porcentaje')
 
-    @app.route('/api/cajeros', methods=['GET'])
-    def get_cajeros():
-        cajeros = Cajero.query.all()
-        return jsonify([cajero.serialize() for cajero in cajeros])
+            if 'ml_g' not in body:
+                list_errors.append('ml_g is required')
+            else:
+                ml_g = body.get('ml_g')
 
+            if len(list_errors) > 0:
+                error_code = 400
+            else:
+                receta = Receta(medicamento=medicamento, tipo_de_toma=tipo_de_toma, cantidad=cantidad,
+                                unidad_medida=unidad_medida, porcentaje=porcentaje, ml_g=ml_g)
+                db.session.add(receta)
+                db.session.commit()
+                receta_id_created = receta.id
 
-    @app.route('/api/cajeros', methods=['POST'])
-    def create_cajero():
-        data = request.get_json()
-        nuevo_cajero = Cajero(**data)
-        db.session.add(nuevo_cajero)
-        db.session.commit()
-        return jsonify(nuevo_cajero.serialize()), 201
+        except Exception as e:
+            db.session.rollback()
+            print("error: ", e)
+            print("sys.exc_info(): ", sys.exc_info())
+            error_code = 500
 
+        if error_code == 400:
+            return jsonify({'success': False,
+                            'message': 'Error creating Receta',
+                            'errors': list_errors}), error_code
+        else:
+            return jsonify({'success': True,
+                            'id': str(receta_id_created),
+                            'message': 'Receta created successfully'
+                            }), 201
 
-    @app.route('/api/cajeros/<int:cajero_id>', methods=['PATCH'])
-    def update_cajero(cajero_id):
-        data = request.get_json()
-        cajero = Cajero.query.get(cajero_id)
-        if cajero is None:
-            return jsonify({'error': 'Cajero no encontrado'}), 404
-        for key, value in data.items():
-            setattr(cajero, key, value)
-        db.session.commit()
-        return jsonify(cajero.serialize())
+    @app.route('/recetas', methods=['GET'])
+    def get_recetas():
+        error_code = 200
+        try:
+            recetas = Receta.query.all()
 
+        except Exception as e:
+            print("error: ", e)
+            print("sys.exc_info(): ", sys.exc_info())
+            db.session.rollback()
+            error_code = 500
 
-    @app.route('/api/cajeros/<int:cajero_id>', methods=['DELETE'])
-    def delete_cajero(cajero_id):
-        cajero = Cajero.query.get(cajero_id)
-        if cajero is None:
-            return jsonify({'error': 'Cajero no encontrado'}), 404
-        db.session.delete(cajero)
-        db.session.commit()
-        return jsonify({'success': True})
+        else:
+            return jsonify({'success': True,
+                            'recetas': [e.serialize() for e in recetas],
+                            'total': len(recetas)
+                            }), 200
 
+    @users_bp.route('/usuarios', methods=['POST'])
+    def create_user():
+        error_lists = []
+        returned_code = 201
+        try:
+            body = request.get_json()
 
-    @app.route('/api/delivery', methods=['GET'])
-    def get_deliverys():
-        deliverys = Delivery.query.all()
-        return jsonify([delivery.serialize() for delivery in deliverys])
+            if 'nombre' not in body:
+                error_lists.append('nombre is required')
+            else:
+                nombre = body.get('nombre')
 
+            if 'password_hash' not in body:
+                error_lists.append('password_hash is required')
+            else:
+                password_hash = body.get('password_hash')
 
-    @app.route('/api/delivery', methods=['POST'])
-    def create_delivery():
-        data = request.get_json()
-        nuevo_delivery = Delivery(**data)
-        db.session.add(nuevo_delivery)
-        db.session.commit()
-        return jsonify(nuevo_delivery.serialize()), 201
+            if 'email' not in body:
+                error_lists.append('email is required')
+            else:
+                email = body.get('email')
 
+            user_db = Usuario.query.filter_by(nombre=nombre).first()
 
-    @app.route('/api/delivery/<int:delivery_id>', methods=['PATCH'])
-    def update_delivery(delivery_id):
-        data = request.get_json()
-        delivery = Delivery.query.get(delivery_id)
-        if delivery is None:
-            return jsonify({'error': 'Delivery no encontrado'}), 404
-        for key, value in data.items():
-            setattr(delivery, key, value)
-        db.session.commit()
-        return jsonify(delivery.serialize())
+            if user_db is not None:
+                if user_db.nombre == nombre:
+                    error_lists.append(
+                        'An account with this username already exists')
+            else:
+                if len(password_hash) < 8:
+                    error_lists.append(
+                        'Password must have at least 8 characters')
 
+            if len(error_lists) > 0:
+                returned_code = 400
+            else:
+                user = Usuario(
+                    nombre=nombre, password_hash=password_hash, email=email)
+                user.insert()
+                user_created_id = user.id
 
-    @app.route('/api/delivery/<int:delivery_id>', methods=['DELETE'])
-    def delete_delivery(delivery_id):
-        delivery = Delivery.query.get(delivery_id)
-        if delivery is None:
-            return jsonify({'error': 'Delivery no encontrado'}), 404
-        db.session.delete(delivery)
-        db.session.commit()
-        return jsonify({'success': True})
+                token = jwt.encode(
+                    {
+                        'user_created_id': user_created_id,
+                        'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=30)
+                    },
+                    config['SECRET_KEY'],
+                    config['ALGORITHM']
+                )
+
+        except Exception as e:
+            print('e:', e)
+            returned_code = 500
+
+        if returned_code == 400:
+            return jsonify({
+                'success': False,
+                'errors': error_lists,
+                'message': 'Error creating a new user'
+            }), returned_code
+        elif returned_code != 201:
+            abort(returned_code)
+        else:
+            return jsonify({
+                'success': True,
+                'token': token,
+                'user_created_id': user_created_id
+            }), returned_code
+
+    @users_bp.route('/api/signin', methods=['POST'])
+    def login():
+        pass
+
+    @users_bp.route('/usuarios/<usuario_id>', methods=['DELETE'])
+    def delete_user(usuario_id):
+        returned_code = 200
+
+        try:
+            user = Usuario.query.get(usuario_id)
+            if user is None:
+                returned_code = 404
+
+            user.delete()
+        except Exception as e:
+            print('e:', e)
+            returned_code = 500
+
+        if returned_code != 200:
+            abort(returned_code)
+        else:
+            return jsonify({
+                'success': True
+            }), returned_code
 
     return app
